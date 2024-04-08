af88951ae1970cc8316b6b09a7f285b3b67ab9e9
Hemant-Dev <phemant092@gmail.com>
2024-04-08 17:49:05 +0530

    basic done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { UserComponent } from "./Components/user/user.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, UserComponent, RouterModule]
 })
 export class AppComponent {
   title = 'Membership-Portal-Frontend';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './Components/user/user.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { WelcomeComponent } from './Components/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: WelcomeComponent
+    },
+    {
+        path: 'user',
+        component: UserComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
