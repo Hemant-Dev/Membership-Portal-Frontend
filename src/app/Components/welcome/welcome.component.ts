48f51af4104c79f649dd8eb1df157ff0a2843f3a
Hemant-Dev <phemant092@gmail.com>
2024-04-09 23:01:59 +0530

    integrated toastr in the app

--- src/app/Components/welcome/welcome.component.ts
+++ src/app/Components/welcome/welcome.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.css'
+  styleUrl: './welcome.component.css',
 })
 export class WelcomeComponent {
+  constructor(private toastr: ToastrService) {}
 
+  showSuccess() {
+    this.toastr.success('Hello World', 'Toastr fun!');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-top',
+    }),
+  ],
 };
